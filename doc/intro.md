75b514b0f1e1511e962e5c5df6720a610aaabf7b
Ingvar Listard <vlzvoice@ya.ru>
2024-09-14 00:08:21 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to retroboard/retroboard
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/retroboard/retroboard_test.clj
@@ -0,0 +1,7 @@
+(ns retroboard.retroboard-test
+  (:require [clojure.test :refer :all]
+            [retroboard.retroboard :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
