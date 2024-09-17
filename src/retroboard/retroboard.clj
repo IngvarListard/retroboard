c8c4d05302677b79906cfb4cf38e32726eeceacc
Ingvar Listard <vlzvoice@ya.ru>
2024-09-18 00:45:27 +0300

    draft and static

--- src/retroboard/retroboard.clj
+++ src/retroboard/retroboard.clj
@@ -1,12 +1,52 @@
 (ns retroboard.retroboard
+  (:require [compojure.core :as c]
+            [compojure.route :as route]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.util.response :as r]
+            [hiccup2.core :as h]
+            [retroboard.ui.components.home :refer [home-page]]
+            [retroboard.ui.core :refer [page]])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(c/defroutes app-routes
+  (c/GET "/" [] (r/response (str (h/html (page (home-page))))))
+  ;; (c/GET "/api/forms/create-event" [] (ajax-views/get-create-event-view))
+  ;; (c/POST "/api/forms/create-event" [] (fn [{:keys [form-params] :as _request}]
+  ;;                                        (create-event! form-params)))
+  ;; (c/GET "/api/forms/create-event/object-form"
+  ;;   {form-params :query-string}
+  ;;   (fn [& _] (-> form-params
+  ;;                 form-decode
+  ;;                 keywordize-keys
+  ;;                 :select-object
+  ;;                 ajax-views/get-record-form-view)))
+  ;; (c/GET "/calendar" [] (r/response (calendar)))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defonce server
+  (run-jetty
+   (-> #'app-routes
+       wrap-reload
+       wrap-json-response
+       wrap-params)
+   {:port 3000
+    :async? true
+    :join? false}))
+
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+  [& _]
+  (println 1234)
+  )
+
+
+(comment
+  :rcf)
